1dd493ac92ac7eadf92399b34e9905f43857e7ad
VikaAndr <vika.d.andrienko@gmail.com>
2026-01-20 14:45:51 +0300

    взяла из интернета программу для проверки I2C, видимо он не работает также написала базовый скетч для RGB (пока не проверен)

--- firmware/VL530X/VL530X.ino
+++ firmware/VL530X/VL530X.ino
@@ -1,23 +1,56 @@
 #include <Wire.h>
 #include "Adafruit_VL53L0X.h"
 
-Adafruit_VL53L0X lox = Adafruit_VL53L0X();
+Adafruit_VL53L0X lox;
 
 void setup() {
   Serial.begin(115200);
-  while (!lox.begin()) {
-    delay(1);
+  while (!Serial);
+
+  Serial.println("Start");
+
+  Wire.begin(); // для Mega важно явно
+
+  if (!lox.begin()) {
+    Serial.println("VL53L0X NOT FOUND");
+    while (1);
   }
+
+  Serial.println("VL53L0X OK");
 }
 
 void loop() {
-  VL53L0X_RangingMeasurementData_t measure;
-  
-  lox.rangingTest(&measure, false);
-  
-  if (measure.RangeStatus != 4) {
-    Serial.println(measure.RangeMilliMeter);
-  }
-  
-  delay(100);
 }
+
+/*
+#include <Wire.h>
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+  Wire.begin();
+  Serial.println("I2C scan...");
+}
+
+void loop() {
+  byte error, address;
+  int found = 0;
+
+  for (address = 1; address < 127; address++) {
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+
+    if (error == 0) {
+      Serial.print("Found at 0x");
+      Serial.println(address, HEX);
+      found++;
+    }
+  }
+
+  if (found == 0) {
+    Serial.println("No I2C devices found");
+  }
+
+  delay(3000);
+}
+*/
